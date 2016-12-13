6be97b89d1a99e22f26d4ef2884ba603fb5bfdc5
tkaessmann <tobias.kaessmann@s24.com>
2016-12-13 16:08:21 +0100

    [solr] Updates Solr version

--- src/main/java/com/s24/search/solr/analyzers/AnalyzingSentenceTokenizer.java
+++ src/main/java/com/s24/search/solr/analyzers/AnalyzingSentenceTokenizer.java
@@ -9,7 +9,7 @@ import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
-import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.util.AttributeFactory;
 
 import com.google.common.base.CharMatcher;
